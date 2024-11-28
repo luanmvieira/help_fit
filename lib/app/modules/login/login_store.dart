6e083910552f784b736156d846d25dde446a4748
Luan Vieira <luanmarcos.vieiradeazevedo@gmail.com>
2024-11-28 20:08:00 -0300

    mudanças login

--- lib/app/modules/login/login_store.dart
+++ lib/app/modules/login/login_store.dart
@@ -1,4 +1,3 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:hep_fit/app/models/user_model.dart';
